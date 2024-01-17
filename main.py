3ec428703f0b6ebcde4352a0ed8e57117e95e995
Sylwester Sojka <sylwestersojka@Sylwesters-MacBook-Air.local>
2024-01-17 20:26:38 +0100

    Showing File Path works, All amount of TOP works also

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter import StringVar, filedialog
 
 # Tkinter
 window = tk.Tk()
-window.geometry('500x500')
+window.geometry('700x500')
 window.title('TOP Flag Tool')
 
 # Labels
@@ -14,6 +14,9 @@ label.grid(row=0, column=0, pady=5)
 label_file_path = tk.Label(window, text="Selected file from: ")
 label_file_path.grid(row=1, column=1, pady=5)
 
+label_for_confirmation = tk.Label(window, text=f'test', width=30)
+label_for_confirmation.grid(row=2, column=0, columnspan=2)
+
 #OptionBar
 selected_option = StringVar(window)
 options = ["Load CSV file", "Quit Program"]
@@ -21,35 +24,6 @@ selected_option.set(options[0])
 
 # Global var
 csv_df = None
-
-# Execute option commands
-def execute_option(*args):
-    global csv_df
-
-    if selected_option.get() == options[0]:
-        file_path = filedialog.askopenfilename(title="Select a CSV file", filetypes=[("CSV files", "*.csv")])
-        if file_path:
-            print(f"Following file was selected: {file_path}")
-            csv_df = pd.read_csv(file_path, delimiter=',')
-            
-            
-    elif selected_option.get() == options[1]:
-        print('Selected QUIT')
-        window.destroy()
-
-dropdown_menu = tk.OptionMenu(window, selected_option, *options, command=execute_option)
-dropdown_menu.grid(row=1, column=0, pady=5)
-
-
-window.mainloop()
-
-
-
-
-# print(list(csv_df.columns))
-
-# pronto_count = csv_df['Pronto ID'].count()
-# print(f'Number of prontos: {pronto_count}')
 
 #Column Definition
 GIC = 'Group in Charge'
@@ -60,9 +34,11 @@ PRONTO = 'Pronto ID'
 TOP_FLAG_VALUE = 'TOP1_24R'
 
 def calculate_tops():
-    result = csv_df[csv_df[TOP_FLAG].str.contains(TOP_FLAG_VALUE, na=False, case=False)]
-    return result
-    
+    if csv_df:
+        result = csv_df[csv_df[TOP_FLAG].str.contains(TOP_FLAG_VALUE, na=False, case=False)]
+        print(result)
+        return result
+
 
 def calculate_top_for_gic(result = calculate_tops(), selected_gic=None):
     result = csv_df[csv_df[TOP_FLAG].str.contains(TOP_FLAG_VALUE, na=False, case=False)]
@@ -71,6 +47,40 @@ def calculate_top_for_gic(result = calculate_tops(), selected_gic=None):
     for gic, count in gic_counts.items():
         if (selected_gic is None or gic == selected_gic) and count >= 3:
             print(f"{gic}: {count}")
-         
+
+
+def top1_flag_counter():
+    top_flag_count = csv_df['TOP']
+
+def show_frontline():
+    pronto_count = csv_df['Pronto ID'].count()
+    # top1_count = calculate_tops()
+    label_for_confirmation["text"] = f'Number of prontos: {pronto_count}'
+
+    
+    
+
+# Execute option commands
+def execute_option(*args):
+    global csv_df
+
+    if selected_option.get() == options[0]:
+        file_path = filedialog.askopenfilename(title="Select a CSV file", filetypes=[("CSV files", "*.csv")])
+        if file_path:
+            print(f"Following file was selected: {file_path}")
+            csv_df = pd.read_csv(file_path, delimiter=',')
+            label_file_path["text"] = f"CSV file: {file_path}"
+            show_frontline()
+            
+            
+    elif selected_option.get() == options[1]:
+        print('Selected QUIT')
+        window.destroy()
+
+dropdown_menu = tk.OptionMenu(window, selected_option, *options, command=execute_option)
+dropdown_menu.grid(row=1, column=0, pady=5)
+
+window.mainloop()
+
 
 
